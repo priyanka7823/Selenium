9fb399970cffc6a3aca97523b0bb6bcc81da0858
priyanka <priynakaavhad7823@gmail.com>
2023-08-24 20:47:06 +0530

    By Mahesh

---
+++ src/test/java/gitpractice/Mahesh.java
@@ -0,0 +1,12 @@
+package gitpractice;
+
+import io.opentelemetry.exporter.logging.SystemOutLogRecordExporter;
+
+public class Mahesh {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("done by Mahesh");
+	}
+
+}
